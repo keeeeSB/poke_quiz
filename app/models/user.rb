d91e57437573f5aaf116c40baa5ea754ca53ed2b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-26 10:10:53 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
