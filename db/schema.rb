f6790cb21a7eb0d20fe5b1623a22314be313a2ec
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-27 13:03:36 +0900

    emailカラムにユニークindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_26_010334) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_27_040226) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_26_010334) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
