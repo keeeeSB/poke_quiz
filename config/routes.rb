115a9427ab3f541c5c0667eb1fcc780607f4c803
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-28 14:05:27 +0900

    Sessionsコントローラーの作成、ヘルパーメソッドを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "users#new"
-  get "/signup",  to: "users#new"
-  post "/signup", to: "users#create"
+  get "/signup",   to: "users#new"
+  post "/signup",  to: "users#create"
+  get "/login",    to: "sessions#new"
+  post "/login",   to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, only: [:show]
   resources :acccount_activations, only: [:edit]
 end
