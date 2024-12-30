ecdd114e9e95fd83b629bfa050bbd3394d18d581
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-30 11:18:29 +0900

    プロフィール画像のアルゴリズムをヘルパーに追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def profile_image_tag(user, options = {})
+    image_class = options[:class] || "img-thumbnail profile-image"
+    if user.profile_image.present?
+      image_tag(user.profile_image, class: image_class, alt: "#{user.name}のプロフィール画像")
+    else
+      image_tag("sample.png", class: image_class, alt: "デフォルトのプロフィール画像")
+    end
+  end
 end
