6ec0b734ce764c6260bde79b1c86659d8022c61c
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-30 11:49:55 +0900

    ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root "users#new"
-  get "/signup",   to: "users#new"
-  post "/signup",  to: "users#create"
-  get "/login",    to: "sessions#new"
-  post "/login",   to: "sessions#create"
-  delete "logout", to: "sessions#destroy"
+  get "/signup",    to: "users#new"
+  post "/signup",   to: "users#create"
+  get "/login",     to: "sessions#new"
+  post "/login",    to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: [:show]
   resources :account_activations, only: [:edit]
 end
