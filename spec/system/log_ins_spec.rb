fd2e0f02558c4c4b458543ac1e119d7bb01c3381
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-31 17:49:40 +0900

    ログインのテストを編集

--- spec/system/log_ins_spec.rb
+++ spec/system/log_ins_spec.rb
@@ -23,7 +23,23 @@ RSpec.describe "ログイン機能", type: :system do
     fill_in "パスワード", with: "wrongpassword"
     click_button "ログイン"
 
-    expect(page).to have_content "メールアドレス、又はパスワードが間違っています。"
+    expect(page).to \
+      have_content "メールアドレス、又はパスワードが間違っています。"
     expect(current_path).to eq login_path
+  end
+
+  scenario "アカウント承認がされていないユーザーはログインに失敗する" do
+    user.update(activated: false)
+
+    visit root_path
+    click_link "ログイン"
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect(page).to \
+      have_content "アカウントが承認されていません。メールを確認してください。"
+    expect(current_path).to eq root_path
   end
 end
