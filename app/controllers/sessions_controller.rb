56d659578f5ab95b8fa64abbaa6f1fe7c98f3950
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-31 10:33:39 +0900

    Sessionsコントローラーの修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      reset_session
-      log_in user
-      flash[:success] = "ログインしました！"
-      redirect_to user
+      if user.activated?
+        reset_session
+        log_in user
+        flash[:success] = "ログインしました！"
+        redirect_to user
+      else
+        flash[:danger] = "アカウントが承認されていません。メールを確認してください。"
+        redirect_to root_path
+      end
     else
-      flash[:danger] = "アカウントが承認されていません。メールを確認してください。"
-      redirect_to root_path
+      flash.now[:danger] = "メールアドレス、又はパスワードが間違っています。"
+      render :new, status: :unprocessable_entity
     end
   end
 
