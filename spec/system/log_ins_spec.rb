f1a9eaa473659f8c991b62d94ad046e0dcb5a02d
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2024-12-31 10:34:04 +0900

    ログイン機能のテストを作成

---
+++ spec/system/log_ins_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "ログイン機能", type: :system do
+  let(:user) { FactoryBot.create(:user) }
+
+  scenario "ユーザーが正しい情報でログインに成功する" do
+    visit root_path
+    click_link "ログイン"
+    
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect(page).to have_content "ログインしました！"
+    expect(current_path).to eq user_path(user)
+  end
+
+  scenario "ユーザーが不正な情報でログインに失敗する" do
+    visit root_path
+    click_link "ログイン"
+
+    fill_in "メールアドレス", with: "wrong@example.com"
+    fill_in "パスワード", with: "wrongpassword"
+    click_button "ログイン"
+
+    expect(page).to have_content "メールアドレス、又はパスワードが間違っています。"
+    expect(current_path).to eq login_path
+  end
+end
